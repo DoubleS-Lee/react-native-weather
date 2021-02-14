3aae74814e108ffd0314dc2fe6868a758111efd7
DoubleS-Lee <lss8825@gmail.com>
2021-02-14 19:14:16 +0900

    1.2:expo API 활용하기

--- App.js
+++ App.js
@@ -1,8 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Loading from './Loading';
+import * as Location from 'expo-location';
 
 export default function App() {
+  
+  const getLocation = async() => {
+    let location = await Location.getCurrentPositionAsync(); 
+    console.log(location);
+  }
+
+  useEffect(() => {
+    getLocation();
+  },[])
+  
   return (
     <Loading />
   );
