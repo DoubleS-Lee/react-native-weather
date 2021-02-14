8a665910e04546f95902d0b5605a1f74f6496880
DoubleS-Lee <lss8825@gmail.com>
2021-02-14 21:24:01 +0900

    2.2:expo에서 각종 아이콘 불러오기

--- Weather.js
+++ Weather.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import PropTypes from 'prop-types';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 
 export default function Weather ({temp, condition}) {
     return (
         <View style={styles.container}>
-            <Text>
-                {temp}
-            </Text>
-            <Text>
-                {condition}
-            </Text>
+            <View style={styles.halfContainer}>
+                <MaterialCommunityIcons name="weather-lightning-rainy" size={96} />
+                <Text style={styles.temp}>{temp}도</Text>
+            </View>
+            <View style={styles.halfContainer}>
+                <Text>{condition}</Text>
+            </View>
         </View>
     )
 }
@@ -21,6 +23,14 @@ const styles = StyleSheet.create({
         flex:1,
         justifyContent: "center",
         alignItems: "center",
+    },
+    halfContainer: {
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+    },
+    temp: {
+        fontSize: 42,
     }
 })
 
