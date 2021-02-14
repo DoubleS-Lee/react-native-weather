bf0923c5b93130353b66cfe1cbeb0ec10085287e
DoubleS-Lee <lss8825@gmail.com>
2021-02-14 20:37:13 +0900

    1.4:api키를 이용하여 데이터 가져오기

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from 'react';
 import Loading from './Loading';
 import * as Location from 'expo-location';
 import { Alert } from 'react-native';
+import axios from 'axios';
+
+const API_KEY = "774ff742b0f8d403ffaebf8f5661f843";
 
 export default function App() {
 
@@ -9,12 +12,18 @@ export default function App() {
     isLoading: true,
   })
   
+  const getWeather = async(latitude, longitude) => {
+    const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+    console.log(data);
+  }
+
   const getLocation = async() => {
     try {
       // throw(error)
       await Location.requestPermissionsAsync();
       const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync(); 
       // console.log(coords);
+      getWeather(latitude, longitude);
       setState({isLoading:false,});
     } catch(error) {
       Alert.alert("위치를 찾을 수가 없습니다", "위치 정보 사용 요청을 승인해주세요")
