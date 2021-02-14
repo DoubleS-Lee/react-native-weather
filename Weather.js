8d8c0688142249d4c8287f4cc0a71584fb92594b
DoubleS-Lee <lss8825@gmail.com>
2021-02-14 21:47:48 +0900

    2.3:expo-linear-gradient사용 및 arry 생성

--- Weather.js
+++ Weather.js
@@ -1,20 +1,71 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, StatusBar } from 'react-native';
 import PropTypes from 'prop-types';
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
 
+const weatherOptions = {
+    Haze: {
+      iconName: "weather-hail",
+      gradient: ["#4DA0B0", "#D39D38"]
+    },
+    Thunderstorm: {
+      iconName: "",
+      gradient: []
+    },
+    Drizzle: {
+      iconName: "",
+      gradient: []
+    },
+    Rain: {
+      iconName: "",
+      gradient: []
+    },
+    Snow: {
+      iconName: "",
+      gradient: []
+    },
+    Atmosphere: {
+      iconName: "",
+      gradient: []
+    },
+    Clear: {
+      iconName: "weather-hail",
+      gradient: ["#4DA0B0", "#D39D38"]
+    },
+    Clouds: {
+      iconName: "",
+      gradient: []
+    },
+    Haze: {
+      iconName: "",
+      gradient: []
+    },
+    Mist: {
+      iconName: "",
+      gradient: []
+    },
+    Dust: {
+      iconName: "",
+      gradient: []
+    }
+  };
 
 export default function Weather ({temp, condition}) {
     return (
-        <View style={styles.container}>
+        <LinearGradient
+            colors={weatherOptions[condition].gradient}
+            style={styles.container}
+        >
             <View style={styles.halfContainer}>
-                <MaterialCommunityIcons name="weather-lightning-rainy" size={96} />
+                <MaterialCommunityIcons name={weatherOptions[condition].iconName} size={96} color='white'/>
+                <StatusBar barStyle="light-content" />
                 <Text style={styles.temp}>{temp}도</Text>
             </View>
             <View style={styles.halfContainer}>
                 <Text>{condition}</Text>
             </View>
-        </View>
+        </LinearGradient>
     )
 }
 
@@ -31,6 +82,7 @@ const styles = StyleSheet.create({
     },
     temp: {
         fontSize: 42,
+        color: 'white',
     }
 })
 
