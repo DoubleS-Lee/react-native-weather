3bbaff1f63a981b487476c102913591b10e6b4de
DoubleS-Lee <lss8825@gmail.com>
2021-02-14 20:14:04 +0900

    1.3:permission 요청 및 위치정보 획득

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Loading from './Loading';
 import * as Location from 'expo-location';
+import { Alert } from 'react-native';
 
 export default function App() {
+
+  const [state, setState] = useState({
+    isLoading: true,
+  })
   
   const getLocation = async() => {
-    let location = await Location.getCurrentPositionAsync(); 
-    console.log(location);
+    try {
+      // throw(error)
+      await Location.requestPermissionsAsync();
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync(); 
+      // console.log(coords);
+      setState({isLoading:false,});
+    } catch(error) {
+      Alert.alert("위치를 찾을 수가 없습니다", "위치 정보 사용 요청을 승인해주세요")
+    }
   }
 
   useEffect(() => {
     getLocation();
   },[])
   
+  const {isLoading} = state;
   return (
-    <Loading />
+    <>
+    {isLoading ? <Loading /> : null}
+    </>
   );
 }
